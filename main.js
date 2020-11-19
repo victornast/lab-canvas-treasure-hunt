0705dab903195da4beacb9477d770305122239cd
Victor Nastasa <victornast@gmail.com>
2020-11-19 21:30:04 +0100

    iterations 2 through 4

--- main.js
+++ main.js
@@ -20,10 +20,72 @@ function drawGrid() {
   }
 }
 
+// Iteration 2 - The Character Class
+class Character {
+  constructor(col, row) {
+    this.col = col;
+    this.row = row;
+  }
+  moveUp() {
+    this.row--;
+  }
+  moveDown() {
+    this.row++;
+  }
+  moveLeft() {
+    this.col--;
+  }
+  moveRight() {
+    this.col++;
+  }
+}
+
+const player = new Character(0, 0);
+
+// Iteration 3 - Drawing the Player
+const playerImg = new Image();
+playerImg.src = 'images/character-down.png';
+
+function drawPlayer() {
+  playerImg.addEventListener('load', () => {
+    context.drawImage(playerImg, player.col * 50 + 1, player.row * 50 + 1);
+  });
+}
+
+// Iteration 4 - The Treasure Class
+class Treasure {
+  // constructor(col, row) {
+  // this.col = col;
+  // this.row = row;
+
+  setRandomPosition() {
+    this.col = Math.floor(Math.random() * 10);
+    this.row = Math.floor(Math.random() * 10);
+  }
+}
+
+const treasureChest = new Treasure();
+treasureChest.setRandomPosition();
+
+const treasureImg = new Image();
+treasureImg.src = 'images/treasure.png';
+
+function drawTreasure() {
+  treasureImg.addEventListener('load', () => {
+    context.drawImage(
+      treasureImg,
+      treasureChest.col * 50 + 1,
+      treasureChest.row * 50 + 1,
+      48,
+      48
+    );
+  });
+}
+
 function drawEverything() {
   drawGrid();
-  // drawPlayer()
-  // drawTreasure()
+  drawPlayer();
+  drawTreasure();
 }
 
 drawEverything();
